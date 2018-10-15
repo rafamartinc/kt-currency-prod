d83ac320164e418ed94d2c2e62f2f9710a8771d1
Rafael Martín-Cuevas Redondo <r.martinc@outlook.com>
2018-10-15 18:09:14 +0200

    Code cropped

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from urllib import request, error
+from urllib import request
 import datetime
 import json
 import time
@@ -99,45 +99,6 @@ class CryptoApi:
         return result
 
 
-def histominute_dict_to_dataset(dataset, dict):
-    result = None
-    for data in dict['Data']:
-
-        if result is None:
-            result = int(data['time']) - 60
-
-        dataset[float(data['time'])] = {
-
-            'open': float(data['open']),
-            'close': float(data['close']),
-            'high': float(data['high']),
-            'low': float(data['low']),
-            'volumefrom': float(data['volumefrom']),
-            'volumeto': float(data['volumeto'])
-        }
-
-    return result
-
-
-def insert_to_dataset(dataset, input_file):
-    data = input_file.readline()
-
-    while data:
-        data = data.split(";")
-
-        dataset[float(data[0])] = {
-
-            'open': float(data[1]),
-            'close': float(data[2]),
-            'high': float(data[3]),
-            'low': float(data[4]),
-            'volumefrom': float(data[5]),
-            'volumeto': float(data[6])
-        }
-
-        data = input_file.readline()
-
-
 api = CryptoApi()
 realConnection = True
 from_symbol = "EUR"
@@ -152,48 +113,3 @@ while True:
     print({datetime.datetime.utcnow().isoformat() + 'Z': results})
 
     time.sleep(10)
-
-
-filename = from_symbol.lower() + to_symbol.lower() + ".csv"
-try:
-    f = open(filename)
-except IOError:
-    print("[WARNING] File not found when trying to read the data.")
-else:
-    f.readline()
-    insert_to_dataset(ds, f)
-
-if realConnection:
-    # If real Internet connection is allowed, download data using the API.
-    dict = api.histominute(from_symbol, to_symbol)
-    previousTimestamp = histominute_dict_to_dataset(ds, dict)
-
-    keyArray = ds.keys()
-    while previousTimestamp is not None:
-
-        if previousTimestamp in keyArray:
-            previousTimestamp -= 60
-
-        else:
-            dict = api.histominute(from_symbol, to_symbol, ts=previousTimestamp)
-            previousTimestamp = histominute_dict_to_dataset(ds, dict)
-            keyArray = ds.keys()
-
-    f = open(filename, "w")
-    f.write("Timestamp;Open;Close;High;Low;Volume From;Volume To\n")
-
-    for t in sorted(ds.keys()):
-        str_output = str(t) + ";" + \
-                str(ds[t]['open']) + ";" + \
-                str(ds[t]['close']) + ";" + \
-                str(ds[t]['high']) + ";" + \
-                str(ds[t]['low']) + ";" + \
-                str(ds[t]['volumefrom']) + ";" + \
-                str(ds[t]['volumeto']) + "\n"
-        f.write(str_output)
-        print(str_output)
-
-print(str(len(ds)) + " records loaded.")
-"""for timestamp in sorted(dataset.keys()):
-    #print str(datetime.datetime.fromtimestamp(timestamp)) + " ___ " + str(dataset[timestamp])
-    print str(timestamp) + " ___ " + str(dataset[timestamp])"""
