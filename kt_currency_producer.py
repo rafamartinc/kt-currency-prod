752a54782105ce50c8d058bf8e8bdc89338d3028
Rafael Martín-Cuevas Redondo <r.martinc@outlook.com>
2018-10-25 14:05:50 +0200

    Created dedicated file for CryptoApi class

--- kt_currency_producer.py
+++ kt_currency_producer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Loads currency data from APIs and sends it to Kafka..
+Loads currency data from APIs and sends it to Kafka.
 
 This script is meant to be a Kafka producer, responsible for retrieving
 data about a currency's value in real time, and loading it into the system.
@@ -12,132 +12,112 @@ This file is subject to the terms and conditions defined in the file
 'LICENSE.txt', which is part of this source code package.
 """
 
-from urllib import request
 import datetime
-import kafka
 import json
 import time
 import sys
+from kafka import KafkaProducer
+
+from src.cryptoapi import CryptoApi
 
 __author__ = "Rafael Martín-Cuevas, Rubén Sainz"
-__copyright__ = "Copyright 2007, The Cogent Project"
 __credits__ = ["Rafael Martín-Cuevas", "Rubén Sainz"]
 __version__ = "0.1.0"
 __status__ = "Development"
 
 
-class CryptoApi:
+class KingstonProducer:
 
-    def __init__(self):
-        self.__url = "https://min-api.cryptocompare.com/data/"
+    def __init__(self, symbol, reference='EUR', sleep=5):
 
-    def histominute(self, fsym, tsym, sign=False, try_conversion=False,
-                    exchange="CCCAGG", aggregate=1, limit=1440, ts=None):
-        """
-        Get open, high, low, close, volumefrom and volumeto from the each minute historical
-        data. This data is only stored for 7 days, if you need more, use the hourly or daily
-        path. It uses BTC conversion if data is not available because the coin is not
-        trading in the specified currency
+        api = CryptoApi()
+        kafka_producer = connect_kafka_producer()
 
-        :param fsym: String (required). From symbol (currency).
-        :param tsym: String (required). To symbols (currencies).
-        :param sign: Boolean. If set to true, the server will sign the requests.
-        :param try_conversion: Boolean. If set to true, it will try to get the missing
-                values by converting currencies to BTC.
-        :param exchange: Exchange data will be loaded from.
-        :param aggregate: Integer. Number of minutes represented by each entry of the
-                resulting data.
-        :param limit: Integer. Indicates de maximum amount of entries to return, up to
-                1440 per request (24h).
-        :param ts: Timestamp. Timestamp from which to load the data (backwards).
-        :return: Resulting data, as a dictionary.
-        """
+        while True:
+            results = api.price(reference, symbol)
+            for k in results:
+                results[k] = 1 / results[k]
 
-        result = {}  # Default return value.
+            document = {datetime.datetime.utcnow().isoformat() + 'Z': results}
+            print(document)
+            kafka_producer.send('kt_currencies', value=json.dumps(document).encode('utf-8'))
 
-        # Build URL needed to request data from the server.
-        url = self.__url + "histominute?" + \
-            "fsym=" + str(fsym) + "&tsym=" + str(tsym) + "&sign=" + str(sign) + \
-            "&tryConversion=" + str(try_conversion) + "&aggregate=" + str(aggregate) + \
-            "&limit=" + str(limit) + "&e=" + str(exchange)
+            time.sleep(sleep)
 
-        # Add the value 'ts', only if specified.
-        if ts:
-            url += "&toTs=" + str(ts)
 
-        # Connect and request data.
-        with request.urlopen(url) as response:
+def connect_kafka_producer():
+    producer = None
+    try:
+        producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                                 value_serializer=lambda x: x,
+                                 api_version=(0, 10))
+    except Exception as ex:
+        print('[ERROR] Exception while connecting Kafka')
+        print(str(ex))
+    finally:
+        return producer
 
-            html = response.read()
 
-            try:
-                # Convert to Python dictionary.
-                result = json.loads(html)
+def parse_args(args):
 
-                if result.has_key("Response") and result.has_key("Message"):
-                    if result["Response"] == "Error":
-                        print("< Error > " + str(result["Message"]))
+    args_dict = {}
 
-            except ValueError as e:
-                print("< ValueError > " + str(e))
+    if len(args) % 2 != 0:
+        raise Exception('Unexpected number of arguments.')
+    else:
+        new_key = ''
+        for i in range(len(args)):
+            if i % 2 == 0:
+                new_key = args[i].lstrip('--')
+            else:
+                args_dict[new_key] = args[i]
 
-        return result
+    return args_dict
 
-    def price(self, fsym, tsyms, sign=False, try_conversion=False, exchange="CCCAGG"):
-        """
-        Get open, high, low, close, volumefrom and volumeto from the each minute historical
-        data. This data is only stored for 7 days, if you need more, use the hourly or daily
-        path. It uses BTC conversion if data is not available because the coin is not
-        trading in the specified currency
 
-        :param fsym: String (required). From symbol (currency).
-        :param tsyms: List (required). To symbols (currencies).
-        :param sign: Boolean. If set to true, the server will sign the requests.
-        :param try_conversion: Boolean. If set to true, it will try to get the missing
-                values by converting currencies to BTC.
-        :param exchange: Exchange data will be loaded from.
-        :return: Resulting data, as a dictionary.
-        """
+def check_args(args):
 
-        result = {}  # Default return value.
+    mandatory_keys = ['symbol']
+    optional_keys = ['reference', 'sleep', 'kafka_host', 'kafka_port']
 
-        # Build URL needed to request data from the server.
-        url = self.__url + "price?" + \
-            "fsym=" + str(fsym) + "&tsyms=" + ",".join(tsyms) + "&sign=" + str(sign) + \
-            "&tryConversion=" + str(try_conversion) + "&e=" + str(exchange)
+    for k in mandatory_keys:
+        if k not in args:
+            raise Exception('Missing argument: ' + str(k) + '.')
 
-        # Connect and request data.
-        with request.urlopen(url) as response:
+    for k in args:
+        if k not in mandatory_keys and k not in optional_keys:
+            raise Exception('Unexpected argument: ' + str(k) + '.')
 
-            html = response.read()
-
-            try:
-                # Convert to Python dictionary.
-                result = json.loads(html)
-
-            except ValueError as e:
-                print("< ValueError > " + str(e))
-
-        return result
+    if 'reference' not in args:
+        args['reference'] = 'EUR'
+    if 'kafka_host' not in args:
+        args['kafka_host'] = 'localhost'
+    if 'kafka_port' not in args:
+        args['kafka_port'] = '9200'
+    args['sleep'] = 5 if 'sleep' not in args else max(5, int(args['sleep']))
 
 
 def main(args):
 
-    if len(args) not in [1, 2]:
-        print('[ERROR] Usage: python kt_currency_producer.py <from_symbol> [from_symbol]')
+    try:
+
+        args = parse_args(args)
+        check_args(args)
+
+    except Exception as ex:
+
+        print(str(ex))
+        print('Usage: python kt_currency_producer.py' +
+              '\n       --symbol <to_symbol> (e.g.: USD)' +
+              '\n       --reference <from_symbol> (optional, default: EUR)' +
+              '\n       --sleep <seconds> (optional, minimum/default: 5)' +
+              '\n       --kafka_host <ip> (optional, default: localhost)' +
+              '\n       --kafka_port <port> (optional, default: 9200)'
+              '\n       --rollback <minutes> (optional, default/max: 7 days')
+
     else:
-        api = CryptoApi()
-        from_symbol = args[1] if len(args) == 2 else 'EUR'
-        to_symbol = [args[0]]
 
-        while True:
-            results = api.price(from_symbol, to_symbol)
-            for k in results:
-                results[k] = 1 / results[k]
-
-            print({datetime.datetime.utcnow().isoformat() + 'Z': results})
-
-            time.sleep(10)
+        kp = KingstonProducer(args['symbol'], args['reference'], args['sleep'])
 
 
 if __name__ == '__main__':
---
+++ src/cryptoapi.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+"""
+Loads currency data from APIs.
+
+This script is responsible for retrieving data about a currency's value
+in real time, as well as historical data if needed. Will hold connection
+methods to several APIs, and will be responsible for one specific currency.
+
+This file is subject to the terms and conditions defined in the file
+'LICENSE.txt', which is part of this source code package.
+"""
+
+from urllib import request
+import json
+
+__author__ = "Rafael Martín-Cuevas, Rubén Sainz"
+__credits__ = ["Rafael Martín-Cuevas", "Rubén Sainz"]
+__version__ = "0.1.0"
+__status__ = "Development"
+
+
+class CryptoApi:
+
+    def __init__(self):
+        self.__url = "https://min-api.cryptocompare.com/data/"
+
+    def histominute(self, fsym, tsym, sign=False, try_conversion=False,
+                    exchange="CCCAGG", aggregate=1, limit=1440, ts=None):
+        """
+        Get open, high, low, close, volumefrom and volumeto from the each minute historical
+        data. This data is only stored for 7 days, if you need more, use the hourly or daily
+        path. It uses BTC conversion if data is not available because the coin is not
+        trading in the specified currency
+
+        :param fsym: String (required). From symbol (currency).
+        :param tsym: String (required). To symbols (currencies).
+        :param sign: Boolean. If set to true, the server will sign the requests.
+        :param try_conversion: Boolean. If set to true, it will try to get the missing
+                values by converting currencies to BTC.
+        :param exchange: Exchange data will be loaded from.
+        :param aggregate: Integer. Number of minutes represented by each entry of the
+                resulting data.
+        :param limit: Integer. Indicates de maximum amount of entries to return, up to
+                1440 per request (24h).
+        :param ts: Timestamp. Timestamp from which to load the data (backwards).
+        :return: Resulting data, as a dictionary.
+        """
+
+        result = {}  # Default return value.
+
+        # Build URL needed to request data from the server.
+        url = self.__url + "histominute?" + \
+              "fsym=" + str(fsym) + "&tsym=" + str(tsym) + "&sign=" + str(sign) + \
+              "&tryConversion=" + str(try_conversion) + "&aggregate=" + str(aggregate) + \
+              "&limit=" + str(limit) + "&e=" + str(exchange)
+
+        # Add the value 'ts', only if specified.
+        if ts:
+            url += "&toTs=" + str(ts)
+
+        # Connect and request data.
+        with request.urlopen(url) as response:
+
+            html = response.read()
+
+            try:
+                # Convert to Python dictionary.
+                result = json.loads(html)
+
+                if result.has_key("Response") and result.has_key("Message"):
+                    if result["Response"] == "Error":
+                        print("< Error > " + str(result["Message"]))
+
+            except ValueError as e:
+                print("< ValueError > " + str(e))
+
+        return result
+
+    def price(self, fsym, tsyms, sign=False, try_conversion=False, exchange="CCCAGG"):
+        """
+        Get open, high, low, close, volumefrom and volumeto from the each minute historical
+        data. This data is only stored for 7 days, if you need more, use the hourly or daily
+        path. It uses BTC conversion if data is not available because the coin is not
+        trading in the specified currency
+
+        :param fsym: String (required). From symbol (currency).
+        :param tsyms: List (required). To symbols (currencies).
+        :param sign: Boolean. If set to true, the server will sign the requests.
+        :param try_conversion: Boolean. If set to true, it will try to get the missing
+                values by converting currencies to BTC.
+        :param exchange: Exchange data will be loaded from.
+        :return: Resulting data, as a dictionary.
+        """
+
+        result = {}  # Default return value.
+
+        # Build URL needed to request data from the server.
+        url = self.__url + "price?" + \
+              "fsym=" + str(fsym) + "&tsyms=" + ",".join(tsyms) + "&sign=" + str(sign) + \
+              "&tryConversion=" + str(try_conversion) + "&e=" + str(exchange)
+
+        # Connect and request data.
+        with request.urlopen(url) as response:
+
+            html = response.read()
+
+            try:
+                # Convert to Python dictionary.
+                result = json.loads(html)
+
+            except ValueError as e:
+                print("< ValueError > " + str(e))
+
+        return result
